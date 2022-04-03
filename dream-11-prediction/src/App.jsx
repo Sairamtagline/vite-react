e2c5f78af7836c6b0745dccb1fa252d128a0b18c
Sairam Tagline <sairam.tagline@gmail.com>
2022-04-03 21:17:52 +0530

    dream-11 changes

---
+++ dream-11-prediction/src/App.jsx
@@ -0,0 +1,155 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+const totalPoints = 100
+
+const team1 = [
+  {
+    name: "P currency",
+    type: "wk",
+    points: 9.0,
+  },
+  {
+    name: "J soliyn",
+    type: "wk",
+    points: 8.5,
+
+  },
+  {
+    name: "D sammy",
+    type: "AR",
+    points: 9.5,
+
+  },
+  {
+    name: "K Shallow",
+    type: "bat",
+    points: 8.5,
+
+  },
+  {
+    name: "E Lewis",
+    type: "bat",
+    points: 8.5,
+
+  },
+  {
+    name: "M Bascombe",
+    type: "bat",
+    points: 9.5,
+
+  },
+  {
+    name: "R Roberts",
+    type: "bol",
+    points: 9.0,
+
+  },
+  {
+    name: "R Wilkinson",
+    type: "bat",
+    points: 8.0,
+
+  },
+  {
+    name: "C Gonsalves",
+    type: "ar",
+    points: 8.0,
+
+  },
+  {
+    name: "M John",
+    type: "bol",
+    points: 8.5,
+
+  },
+  {
+    name: "D Butler",
+    type: "bol",
+    points: 8.5,
+
+  },
+
+]
+const team2 = [
+  {
+    name: "A Hooper",
+    type: "ar",
+    points: 10.0,
+
+  },
+  {
+    name: "T Harry",
+    type: "wk",
+    points: 8.0,
+
+  },
+  {
+    name: "U Thomas",
+    type: "",
+    points: 0.9,
+
+  },
+  {
+    name: "W Strough",
+    type: "",
+    points: 0.9,
+
+  },
+  {
+    name: "R Hillocks",
+    type: "",
+    points: 0.9,
+
+  },
+  {
+    name: "K Horne",
+    type: "",
+    points: 0.9,
+
+  },
+  {
+    name: "S Williams",
+    type: "",
+    points: 0.9,
+
+  },
+  {
+    name: "S Willaams",
+    type: "",
+    points: 0.9,
+
+  },
+  {
+    name: "A Casesar",
+    type: "",
+    points: 0.9,
+
+  },
+  {
+    name: "L Wilson",
+    type: "",
+    points: 0.9,
+
+  },
+  {
+    name: "R Pichards",
+    type: "",
+    points: 0.9,
+
+  },
+
+]
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      {
+        " hi"
+      }
+    </div>
+  )
+}
+
+export default App
